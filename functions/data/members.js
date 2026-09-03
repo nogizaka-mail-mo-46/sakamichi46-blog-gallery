6dbbe42d1b4a95e2ae1c2a66e1e2d9085553f839
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-03 23:34:42 +0900

    Update members.js

--- functions/data/members.js
+++ functions/data/members.js
@@ -1,11 +1,46 @@
 export const members = {
+    /*
+     * 乃木坂46（3期生）
+     */
     ayanochristie_yoshida: {
         name: "吉田 綾乃クリスティー",
+        group: "nogizaka46",
         folderId: "1rIwQd6qnWUDFZRvHX6wmzobVk_sqsB3q"
     },
 
+    /*
+     * 乃木坂46（5期生）
+     */    
     teresa_ikeda: {
         name: "池田 瑛紗",
+        group: "nogizaka46",
         folderId: "1KAvDzdoaR8_vWChmsBiv_HY3ZfckiYEt"
+    },
+        
+    /*
+     * 櫻坂46（2期生）
+     */
+    marino_kousaka: {
+        name: "幸阪 茉里乃",
+        group: "sakurazaka46",
+        folderId: "1Z8ZosN4iFnL3rbGR7X_x9NYbvPDIaCJi"
+    },
+        
+    /*
+     * 日向坂46（4期生）
+     */
+    yoko-shogenji: {
+        name: "正源司 陽子",
+        group: "hinatazaka46",
+        folderId: "1mt7PO-xBZyh4-yE8aMl9KGAdpkH7S5ck"
+    },
+        
+    /*
+     * 日向坂46（5期生）
+     */
+    yu-sato: {
+        name: "佐藤 優羽",
+        group: "hinatazaka46",
+        folderId: "1wCWo0QvcrGPR7Y5JXV6s0jRzVUHfbu2B"
     }
 };

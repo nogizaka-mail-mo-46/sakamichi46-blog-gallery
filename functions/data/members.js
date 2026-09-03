8727b210e803d97a7080413401828a8ff376192a
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-03 22:56:18 +0900

    Update members.js

--- functions/data/members.js
+++ functions/data/members.js
@@ -1,6 +1,11 @@
 export const members = {
-    yoshida_ayano_christie: {
+    ayanochristie_yoshida: {
         name: "吉田 綾乃クリスティー",
         folderId: "1rIwQd6qnWUDFZRvHX6wmzobVk_sqsB3q"
+    },
+
+    teresa_ikeda: {
+        name: "池田 瑛紗",
+        folderId: "1KAvDzdoaR8_vWChmsBiv_HY3ZfckiYEt"
     }
 };

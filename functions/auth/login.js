805fb13b9dddf72d7d3c616ec14db8a518f3e2df
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-03 18:12:25 +0900

    Update login.js

--- functions/auth/login.js
+++ functions/auth/login.js
@@ -1,1 +1,42 @@
+export async function onRequestGet(context) {
+    const { env } = context;
 
+    const authUrl = new URL(
+        "https://accounts.google.com/o/oauth2/v2/auth"
+    );
+
+    authUrl.searchParams.set(
+        "client_id",
+        env.GOOGLE_CLIENT_ID
+    );
+
+    authUrl.searchParams.set(
+        "redirect_uri",
+        env.GOOGLE_PAGES_REDIRECT_URI
+    );
+
+    authUrl.searchParams.set(
+        "response_type",
+        "code"
+    );
+
+    authUrl.searchParams.set(
+        "scope",
+        "openid email"
+    );
+
+    authUrl.searchParams.set(
+        "access_type",
+        "offline"
+    );
+
+    authUrl.searchParams.set(
+        "prompt",
+        "select_account"
+    );
+
+    return Response.redirect(
+        authUrl.toString(),
+        302
+    );
+}

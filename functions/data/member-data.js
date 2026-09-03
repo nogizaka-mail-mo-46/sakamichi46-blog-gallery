883e0fe2a17161b33d3c1c6344c92f6ea5724729
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-04 03:29:44 +0900

    Update member-data.js

--- functions/data/member-data.js
+++ functions/data/member-data.js
@@ -5,43 +5,43 @@ export const members = {
     ayanochristie_yoshida: {
         name: "吉田 綾乃クリスティー",
         group: "nogizaka46",
-        folderId: "1rIwQd6qnWUDFZRvHX6wmzobVk_sqsB3q"
+        folderId: "1rIwQd6qnWUDFZRvHX6wmzobVk_sqsB3q",
         blogDataFolderId: "1-67kUkIaF4mZjKOJW1mC0sQEH_zbfiCD"
     },
 
-    // /*
-    //  * 乃木坂46（5期生）
-    //  */    
-    // teresa_ikeda: {
-    //     name: "池田 瑛紗",
-    //     group: "nogizaka46",
-    //     folderId: "1KAvDzdoaR8_vWChmsBiv_HY3ZfckiYEt"
-    // },
+    /*
+     * 乃木坂46（5期生）
+     */    
+    teresa_ikeda: {
+        name: "池田 瑛紗",
+        group: "nogizaka46",
+        folderId: "1KAvDzdoaR8_vWChmsBiv_HY3ZfckiYEt"
+    },
         
-    // /*
-    //  * 櫻坂46（2期生）
-    //  */
-    // marino_kousaka: {
-    //     name: "幸阪 茉里乃",
-    //     group: "sakurazaka46",
-    //     folderId: "1Z8ZosN4iFnL3rbGR7X_x9NYbvPDIaCJi"
-    // },
+    /*
+     * 櫻坂46（2期生）
+     */
+    marino_kousaka: {
+        name: "幸阪 茉里乃",
+        group: "sakurazaka46",
+        folderId: "1Z8ZosN4iFnL3rbGR7X_x9NYbvPDIaCJi"
+    },
         
-    // /*
-    //  * 日向坂46（4期生）
-    //  */
-    // yoko_shogenji: {
-    //     name: "正源司 陽子",
-    //     group: "hinatazaka46",
-    //     folderId: "1mt7PO-xBZyh4-yE8aMl9KGAdpkH7S5ck"
-    // },
+    /*
+     * 日向坂46（4期生）
+     */
+    yoko_shogenji: {
+        name: "正源司 陽子",
+        group: "hinatazaka46",
+        folderId: "1mt7PO-xBZyh4-yE8aMl9KGAdpkH7S5ck"
+    },
         
-    // /*
-    //  * 日向坂46（5期生）
-    //  */
-    // yu_sato: {
-    //     name: "佐藤 優羽",
-    //     group: "hinatazaka46",
-    //     folderId: "1wCWo0QvcrGPR7Y5JXV6s0jRzVUHfbu2B"
-    // }
+    /*
+     * 日向坂46（5期生）
+     */
+    yu_sato: {
+        name: "佐藤 優羽",
+        group: "hinatazaka46",
+        folderId: "1wCWo0QvcrGPR7Y5JXV6s0jRzVUHfbu2B"
+    }
 };

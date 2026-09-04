c760de9b57294d30eb9f51d8143aff228f7d1ff4
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-04 12:25:52 +0900

    Update member-data.js

--- functions/data/member-data.js
+++ functions/data/member-data.js
@@ -29,40 +29,40 @@ export const members = {
         blogDataFolderId: "1yE-qJGnw5gp6rWSVmXOpDMzwW96tmzu8"
     },
 
-    /*
-     * 乃木坂46（6期生）
-     */    
-    hinano_okoshi: {
-        name: "大越 ひなの",
-        group: "nogizaka46",
-        folderId: "1_tuGgMBmqmTn_Pct185xaYIekQ_GjHTy",
-        blogDataFolderId: "1xkIkRM0S7ESCkPee52LHUGxZgm2KZzIv"
-    },
+    // /*
+    //  * 乃木坂46（6期生）
+    //  */    
+    // hinano_okoshi: {
+    //     name: "大越 ひなの",
+    //     group: "nogizaka46",
+    //     folderId: "1_tuGgMBmqmTn_Pct185xaYIekQ_GjHTy",
+    //     blogDataFolderId: "1xkIkRM0S7ESCkPee52LHUGxZgm2KZzIv"
+    // },
         
-    /*
-     * 櫻坂46（2期生）
-     */
-    marino_kousaka: {
-        name: "幸阪 茉里乃",
-        group: "sakurazaka46",
-        folderId: "1Z8ZosN4iFnL3rbGR7X_x9NYbvPDIaCJi"
-    },
+    // /*
+    //  * 櫻坂46（2期生）
+    //  */
+    // marino_kousaka: {
+    //     name: "幸阪 茉里乃",
+    //     group: "sakurazaka46",
+    //     folderId: "1Z8ZosN4iFnL3rbGR7X_x9NYbvPDIaCJi"
+    // },
         
-    /*
-     * 日向坂46（4期生）
-     */
-    yoko_shogenji: {
-        name: "正源司 陽子",
-        group: "hinatazaka46",
-        folderId: "1mt7PO-xBZyh4-yE8aMl9KGAdpkH7S5ck"
-    },
+    // /*
+    //  * 日向坂46（4期生）
+    //  */
+    // yoko_shogenji: {
+    //     name: "正源司 陽子",
+    //     group: "hinatazaka46",
+    //     folderId: "1mt7PO-xBZyh4-yE8aMl9KGAdpkH7S5ck"
+    // },
         
-    /*
-     * 日向坂46（5期生）
-     */
-    yu_sato: {
-        name: "佐藤 優羽",
-        group: "hinatazaka46",
-        folderId: "1wCWo0QvcrGPR7Y5JXV6s0jRzVUHfbu2B"
-    }
+    // /*
+    //  * 日向坂46（5期生）
+    //  */
+    // yu_sato: {
+    //     name: "佐藤 優羽",
+    //     group: "hinatazaka46",
+    //     folderId: "1wCWo0QvcrGPR7Y5JXV6s0jRzVUHfbu2B"
+    // }
 };

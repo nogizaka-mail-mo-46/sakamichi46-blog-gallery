54c202437428258967e0e9e8b4d7e15fda5328db
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-12 22:37:27 +0900

    Create sakurazaka-members.js

---
+++ functions/data/sakurazaka-members.js
@@ -0,0 +1,58 @@
+export const sakurazakaMembers = {
+    /*
+     * 櫻坂46（2期生）
+     */
+    marino_kousaka: {
+        name: "幸阪 茉里乃",
+        group: "sakurazaka46",
+        blogDataFolderId: "1KuADtqTDIbC7KyhxPtnmAevxJP-10ba4"
+    },
+    hono_tamura: {
+        name: "田村 保乃",
+        group: "sakurazaka46",
+        blogDataFolderId: "13qb7jPoA-ZLrcXTtHIRXb3CHvTDd3sPO"
+    },
+    kira_masumoto: {
+        name: "増本 綺良",
+        group: "sakurazaka46",
+        blogDataFolderId: "1-QUAtAm7AuYxnSkGlrod-v8o6mD7MEY3"
+    },
+    hikaru_morita: {
+        name: "森田 ひかる",
+        group: "sakurazaka46",
+        blogDataFolderId: "1i_R4nuvKU50oy0YGpDS5IoDSfpNGe0tw"
+    },
+
+    /*
+     * 櫻坂46（3期生）
+     */
+    airi_taniguchi: {
+        name: "谷口 愛季",
+        group: "sakurazaka46",
+        blogDataFolderId: "12cKACtbscK_tkDKfj1XjvAuDz1XCO-4d"
+    },
+    itoha_mukai: {
+        name: "向井 純葉",
+        group: "sakurazaka46",
+        blogDataFolderId: "1ZKIooKsnCnGdj5bmMoCQBYc21-YmO9L1"
+    },
+    shizuki_yamashita: {
+        name: "山下 瞳月",
+        group: "sakurazaka46",
+        blogDataFolderId: "15b0Z3nVf76ZJLYy9UcRdHHHiw0qJivat"
+    },
+
+    /*
+     * 櫻坂46（4期生）
+     */
+    haru_katsumata: {
+        name: "勝又 春",
+        group: "sakurazaka46",
+        blogDataFolderId: "1TI7b0vIKkzpPjNav_Kp-_0jtmVMTOvP6"
+    },
+    wako_matsumoto: {
+        name: "松本 和子",
+        group: "sakurazaka46",
+        blogDataFolderId: "1Tk7NQ5fBRL1yn6OhX-Cwd0zAp5epXyHo"
+    }
+};

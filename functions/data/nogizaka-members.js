3e28503662cdc3c1af8a45a6de499e71f63a1862
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-12 22:37:07 +0900

    Create nogizaka-members.js

---
+++ functions/data/nogizaka-members.js
@@ -0,0 +1,142 @@
+export const nogizakaMembers = {
+    /*
+     * 乃木坂46（3期生）
+     */
+    riria_ito: {
+        name: "伊藤 理々杏",
+        group: "nogizaka46",
+        blogIndexFileId: "1U0CLchMz8_ldP8Bb2kaxnwLojaiX14eR"
+    },
+    enka_iwamoto: {
+        name: "岩本 蓮加",
+        group: "nogizaka46",
+        blogDataFolderId: "1Rey7q9GtvvW9y0P99zj3TjlzGnW9YN9_"
+    },
+    ayanochristie_yoshida: {
+        name: "吉田 綾乃クリスティー",
+        group: "nogizaka46",
+        blogDataFolderId: "1-67kUkIaF4mZjKOJW1mC0sQEH_zbfiCD"
+    },
+
+    /*
+     * 乃木坂46（4期生）
+     */
+    sakura_endo: {
+        name: "遠藤 さくら",
+        group: "nogizaka46",
+        blogDataFolderId: "1SfbkGVB8HcX6mgd6EJ2LVzveOXbJFEWe"
+    },
+    haruka_kaki: {
+        name: "賀喜 遥香",
+        group: "nogizaka46",
+        blogDataFolderId: "1ssFbOUv3p2khtJL6-mZpWfLJgRXBKbJu"
+    },
+    yuna_shibata: {
+        name: "柴田 柚菜",
+        group: "nogizaka46",
+        blogDataFolderId: "1O_ZilVPAae4gtl2jMvUhG0AlAPLzDBIS"
+    },
+    mayu_tamura: {
+        name: "田村 真佑",
+        group: "nogizaka46",
+        blogDataFolderId: "1h1dWtt64ynk6dhxhHTB5sNKu1cO5MJHS"
+    },
+    runa_hayashi: {
+        name: "林 瑠奈",
+        group: "nogizaka46",
+        blogDataFolderId: "1kIfZL0snHrbbuwnu8JyVNhmtIrBmuTdY"
+    },
+
+    /*
+     * 乃木坂46（5期生）
+     */
+    mao_ioki: {
+        name: "五百城 茉央",
+        group: "nogizaka46",
+        blogDataFolderId: "1gHmsk1juS-TiK3ZMxO2BI4NpnexA5UVK"
+    },
+    teresa_ikeda: {
+        name: "池田 瑛紗",
+        group: "nogizaka46",
+        blogDataFolderId: "1yE-qJGnw5gp6rWSVmXOpDMzwW96tmzu8"
+    },
+    miku_ichinose: {
+        name: "一ノ瀬 美空",
+        group: "nogizaka46",
+        blogDataFolderId: "1h6pWs87CEkaUmh5vbdr7XjgZQNBeJwwU"
+    },
+    nagi_inoue: {
+        name: "井上 和",
+        group: "nogizaka46",
+        blogDataFolderId: "17hra8rHJVBUNf0yW08mqHqRZVi-t80t-"
+    },
+    hina_okamoto: {
+        name: "岡本 姫奈",
+        group: "nogizaka46",
+        blogDataFolderId: "166rGlq8UNPrrP2H2JsKsB5Evbx_puiF1"
+    },
+    aya_ogawa: {
+        name: "小川 彩",
+        group: "nogizaka46",
+        blogDataFolderId: "1DlXh9ev8CMUp4ctm8lURbPZ8NdkH-Hsu"
+    },
+    iroha_okuda: {
+        name: "奥田 いろは",
+        group: "nogizaka46",
+        blogDataFolderId: "1Delrb-iSNMYCoopAUTJYFy1YDkpxA6QH"
+    },
+    sakura_kawasaki: {
+        name: "川﨑 桜",
+        group: "nogizaka46",
+        blogDataFolderId: "1HhDq31_Mq1MvqoOUh5uw2gnmQFr3vj_R"
+    },
+    nao_tomisato: {
+        name: "冨里 奈央",
+        group: "nogizaka46",
+        blogDataFolderId: "1pup-2MuqmmjXbnus82LnN2fu5_2au4V1"
+    },
+    aruno_nakanishi: {
+        name: "中西 アルノ",
+        group: "nogizaka46",
+        blogDataFolderId: "1JBC-BgdU_qsYdm29m7Uixc88PXVHzruZ"
+    },
+
+    /*
+     * 乃木坂46（6期生）
+     */
+    kokone_atago: {
+        name: "愛宕 心響",
+        group: "nogizaka46",
+        blogDataFolderId: "1dotZGDH81ZC9DP5BDX2NbsbjA1XUPVLh"
+    },
+    hinano_okoshi: {
+        name: "大越 ひなの",
+        group: "nogizaka46",
+        blogDataFolderId: "1xkIkRM0S7ESCkPee52LHUGxZgm2KZzIv"
+    },
+    akari_kaibe: {
+        name: "海邉 朱莉",
+        group: "nogizaka46",
+        blogDataFolderId: "17w1ALgFsiWyeVYSaMIOgw6DeE3DDASUC"
+    },
+    mitsuki_setoguchi: {
+        name: "瀬戸口 心月",
+        group: "nogizaka46",
+        blogDataFolderId: "1fPmVJoLVY64Sd2kFUlXtJMmrl9nRWv7k"
+    },
+    mirine_masuda: {
+        name: "増田 三莉音",
+        group: "nogizaka46",
+        blogDataFolderId: "10volpFE5XxrHlwotzhUuuGdo5KoVCbhF"
+    },
+    urumi_morihira: {
+        name: "森平 麗心",
+        group: "nogizaka46",
+        blogDataFolderId: "1MS__Hl-Mnlbn9yqQD1v8rJQ-jvK7K4Wn"
+    },
+    moeka_yada: {
+        name: "矢田 萌華",
+        group: "nogizaka46",
+        blogDataFolderId: "14cXP6ce-6VeUvwh2iXJ8Us_YobiVhtcu"
+    }
+};

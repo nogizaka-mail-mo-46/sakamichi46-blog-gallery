a112539e9565eb6af09bcb343934e6d295846ad6
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-12 22:37:42 +0900

    Create hinatazaka-members.js

---
+++ functions/data/hinatazaka-members.js
@@ -0,0 +1,72 @@
+export const hinatazakaMembers = {
+    /*
+     * 日向坂46（2期生）
+     */
+    nao_kosaka: {
+        name: "小坂 菜緒",
+        group: "hinatazaka46",
+        blogDataFolderId: "1rMava3uLkj1txypUh8Pdl3PvvQPJVbb_"
+    },
+
+    /*
+     * 日向坂46（3期生）
+     */
+    marie_morimoto: {
+        name: "森本 茉莉",
+        group: "hinatazaka46",
+        blogDataFolderId: "1L1L3XzlLKhvizoxLMQnqAliBXLUNeM-M"
+    },
+    haruyo_yamaguchi: {
+        name: "山口 陽世",
+        group: "hinatazaka46",
+        blogDataFolderId: "1V2CUrwLFNzMicAsh87WUimkVWQoB2gmN"
+    },
+
+    /*
+     * 日向坂46（4期生）
+     */
+    yoko_shogenji: {
+        name: "正源司 陽子",
+        group: "hinatazaka46",
+        blogDataFolderId: "13DqduBrK69Lcs-WJuRT8coIKwdOStlAq"
+    },
+    honoka_hirao: {
+        name: "平尾 帆夏",
+        group: "hinatazaka46",
+        blogDataFolderId: "1gUzhAWkn4_ncBec3dwMGUQKC1fwaGBta"
+    },
+    kaho_fujishima: {
+        name: "藤嶌 果歩",
+        group: "hinatazaka46",
+        blogDataFolderId: "15cibyoojGmXTXZS1l6iozh0OXJT558if"
+    },
+    rina_watanabe: {
+        name: "渡辺 莉奈",
+        group: "hinatazaka46",
+        blogDataFolderId: "1_20V0Rd2cnIWT1IWawjiH4Z4F_TdkeBd"
+    },
+
+    /*
+     * 日向坂46（5期生）
+     */
+    manami_ono: {
+        name: "大野 愛実",
+        group: "hinatazaka46",
+        blogDataFolderId: "1PEHpjpaSbvUp1MUgih5IlTLRzGX1W2Et"
+    },
+    saki_katayama: {
+        name: "片山 紗希",
+        group: "hinatazaka46",
+        blogDataFolderId: "1mucuCkgnf3ljfIhoeZquMEgHCwm4DGH8"
+    },
+    hinano_kuramori: {
+        name: "蔵盛 妃那乃",
+        group: "hinatazaka46",
+        blogDataFolderId: "1YMU7O6OY3tJF3aHXh_0MlGjQ_1-F4o1-"
+    },
+    yu_sato: {
+        name: "佐藤 優羽",
+        group: "hinatazaka46",
+        blogDataFolderId: "1FsFYu7aC11Emtd9cDYntVPdGJumCh2v9"
+    }
+};

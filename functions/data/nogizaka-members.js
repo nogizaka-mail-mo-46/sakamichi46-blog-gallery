8e68b6351d88b75cbee3664d91145996d15d8fef
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-12 22:52:33 +0900

    Update nogizaka-members.js

--- functions/data/nogizaka-members.js
+++ functions/data/nogizaka-members.js
@@ -10,12 +10,12 @@ export const nogizakaMembers = {
     enka_iwamoto: {
         name: "岩本 蓮加",
         group: "nogizaka46",
-        blogDataFolderId: "1Rey7q9GtvvW9y0P99zj3TjlzGnW9YN9_"
+        blogIndexFileId: "1zkERs7Q-KZVoJQqMPEItbElGCHtdJ7tj"
     },
     ayanochristie_yoshida: {
         name: "吉田 綾乃クリスティー",
         group: "nogizaka46",
-        blogDataFolderId: "1-67kUkIaF4mZjKOJW1mC0sQEH_zbfiCD"
+        blogIndexFileId: "1iSXvv09Zdkqg7qqT-pWZoLyIMacyhthp"
     },
 
     /*
@@ -24,27 +24,27 @@ export const nogizakaMembers = {
     sakura_endo: {
         name: "遠藤 さくら",
         group: "nogizaka46",
-        blogDataFolderId: "1SfbkGVB8HcX6mgd6EJ2LVzveOXbJFEWe"
+        blogIndexFileId: "12W4Uh0g7LZ6Vdv7b8aMhwBboMnaSuXGk"
     },
     haruka_kaki: {
         name: "賀喜 遥香",
         group: "nogizaka46",
-        blogDataFolderId: "1ssFbOUv3p2khtJL6-mZpWfLJgRXBKbJu"
+        blogIndexFileId: "1KGWsIG98kI3be8KviwTAXV6rLt5Jy4Xs"
     },
     yuna_shibata: {
         name: "柴田 柚菜",
         group: "nogizaka46",
-        blogDataFolderId: "1O_ZilVPAae4gtl2jMvUhG0AlAPLzDBIS"
+        blogIndexFileId: "10usYx1tPr_JrnOkcthQbxFLVVOzXJEr4"
     },
     mayu_tamura: {
         name: "田村 真佑",
         group: "nogizaka46",
-        blogDataFolderId: "1h1dWtt64ynk6dhxhHTB5sNKu1cO5MJHS"
+        blogIndexFileId: "1duyUqh1lTzShUszD46Jz8VT0e8AOXZ2q"
     },
     runa_hayashi: {
         name: "林 瑠奈",
         group: "nogizaka46",
-        blogDataFolderId: "1kIfZL0snHrbbuwnu8JyVNhmtIrBmuTdY"
+        blogIndexFileId: "1ItxMEQB0m5EzaoBGQvYlBZ7TAH-EAr25"
     },
 
     /*
@@ -53,52 +53,52 @@ export const nogizakaMembers = {
     mao_ioki: {
         name: "五百城 茉央",
         group: "nogizaka46",
-        blogDataFolderId: "1gHmsk1juS-TiK3ZMxO2BI4NpnexA5UVK"
+        blogIndexFileId: "1RK0bgIDGH0geiBE9mLhzRCAOcxfx-Z4X"
     },
     teresa_ikeda: {
         name: "池田 瑛紗",
         group: "nogizaka46",
-        blogDataFolderId: "1yE-qJGnw5gp6rWSVmXOpDMzwW96tmzu8"
+        blogIndexFileId: "12CAcGZo1uGYLhSYpf6KwG0oSPxTwNxvX"
     },
     miku_ichinose: {
         name: "一ノ瀬 美空",
         group: "nogizaka46",
-        blogDataFolderId: "1h6pWs87CEkaUmh5vbdr7XjgZQNBeJwwU"
+        blogIndexFileId: "1rQvn-tg81jjKML2zXHveEpcB33UDLcCk"
     },
     nagi_inoue: {
         name: "井上 和",
         group: "nogizaka46",
-        blogDataFolderId: "17hra8rHJVBUNf0yW08mqHqRZVi-t80t-"
+        blogIndexFileId: "1jUPRQN2PkZ3xGVQ1CLHNA_jkVXuo-L1I"
     },
     hina_okamoto: {
         name: "岡本 姫奈",
         group: "nogizaka46",
-        blogDataFolderId: "166rGlq8UNPrrP2H2JsKsB5Evbx_puiF1"
+        blogIndexFileId: "1Ts_0gEtqRESJPgsbZbgJWOSbXPmukPZf"
     },
     aya_ogawa: {
         name: "小川 彩",
         group: "nogizaka46",
-        blogDataFolderId: "1DlXh9ev8CMUp4ctm8lURbPZ8NdkH-Hsu"
+        blogIndexFileId: "1N3NewkXq_jvWDf2IxaE9WW7dwhelVcvX"
     },
     iroha_okuda: {
         name: "奥田 いろは",
         group: "nogizaka46",
-        blogDataFolderId: "1Delrb-iSNMYCoopAUTJYFy1YDkpxA6QH"
+        blogIndexFileId: "1wRDWVXQY3Y2kJpkYe-p9OhH6Q8jmf6nj"
     },
     sakura_kawasaki: {
         name: "川﨑 桜",
         group: "nogizaka46",
-        blogDataFolderId: "1HhDq31_Mq1MvqoOUh5uw2gnmQFr3vj_R"
+        blogIndexFileId: "1vGz6HSu6wi6ecJrjq1lhz12MfGGnnEmx"
     },
     nao_tomisato: {
         name: "冨里 奈央",
         group: "nogizaka46",
-        blogDataFolderId: "1pup-2MuqmmjXbnus82LnN2fu5_2au4V1"
+        blogIndexFileId: "1weWVMuwnZ5nffkcqbZarHTYoJQaGZ62S"
     },
     aruno_nakanishi: {
         name: "中西 アルノ",
         group: "nogizaka46",
-        blogDataFolderId: "1JBC-BgdU_qsYdm29m7Uixc88PXVHzruZ"
+        blogIndexFileId: "1cncQhL12H9ektmSpq9LYMYY0MbMjuhnq"
     },
 
     /*
@@ -107,36 +107,36 @@ export const nogizakaMembers = {
     kokone_atago: {
         name: "愛宕 心響",
         group: "nogizaka46",
-        blogDataFolderId: "1dotZGDH81ZC9DP5BDX2NbsbjA1XUPVLh"
+        blogIndexFileId: "1rOCHd7UDuDwfoL4GmthcxTq22rxrCvaX"
     },
     hinano_okoshi: {
         name: "大越 ひなの",
         group: "nogizaka46",
-        blogDataFolderId: "1xkIkRM0S7ESCkPee52LHUGxZgm2KZzIv"
+        blogIndexFileId: "1Di-Nlje9fSsICNGbaYuOaiHmO-QhIc-8"
     },
     akari_kaibe: {
         name: "海邉 朱莉",
         group: "nogizaka46",
-        blogDataFolderId: "17w1ALgFsiWyeVYSaMIOgw6DeE3DDASUC"
+        blogIndexFileId: "1tHJ1cXUj51Ls26RvVQkIzXSJH0CE6eS4"
     },
     mitsuki_setoguchi: {
         name: "瀬戸口 心月",
         group: "nogizaka46",
-        blogDataFolderId: "1fPmVJoLVY64Sd2kFUlXtJMmrl9nRWv7k"
+        blogIndexFileId: "1Q1NF6QXN_UqjJs7reZkBv8AY6W0HiVLq"
     },
     mirine_masuda: {
         name: "増田 三莉音",
         group: "nogizaka46",
-        blogDataFolderId: "10volpFE5XxrHlwotzhUuuGdo5KoVCbhF"
+        blogIndexFileId: "19GGF6zdPrqqQDkn3g3asHWtpW1YFVTad"
     },
     urumi_morihira: {
         name: "森平 麗心",
         group: "nogizaka46",
-        blogDataFolderId: "1MS__Hl-Mnlbn9yqQD1v8rJQ-jvK7K4Wn"
+        blogIndexFileId: "1IdE4XJ0MWoXTllfVHBDfvEKqCj9720l3"
     },
     moeka_yada: {
         name: "矢田 萌華",
         group: "nogizaka46",
-        blogDataFolderId: "14cXP6ce-6VeUvwh2iXJ8Us_YobiVhtcu"
+        blogIndexFileId: "1AMBVmLM_zt3KKR-3wyIxn19N0a-CKpea"
     }
 };

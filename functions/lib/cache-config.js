66c21c9a953e02a076fc8fe7c277373a0bd93b4b
nogizaka-mail-mo-46 <nogizaka.mail.mo@gmail.com>
2026-09-26 18:13:07 +0900

    Update cache-config.js

--- functions/lib/cache-config.js
+++ functions/lib/cache-config.js
@@ -17,6 +17,9 @@ export const CACHE_SECONDS = Object.freeze({
     // /api/blogs: 絞り込み・並び替え後の完成レスポンス
     BLOGS_RESPONSE: 600,
 
+    // /api/blog: ブログ詳細JSON
+    BLOG_DETAIL: 3600,
+
     // /api/member-icons: グループ別アイコン一覧
     MEMBER_ICONS: 3600
 });
